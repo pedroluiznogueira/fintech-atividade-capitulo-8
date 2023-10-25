d7f77a3c611d476270d50c756c626db08131524a
pedroluiznogueira <peluiznogueiram@hotmail.com>
2023-10-25 19:23:49 -0300

    adding error handling for insert and getAll

--- fintech/dao/TransacaoDAO.java
+++ fintech/dao/TransacaoDAO.java
@@ -9,33 +9,42 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class TransacaoDAO {
-    public void insert(Connection conexao, Transacao transacao) throws SQLException {
-        // Preparando o sql insert
-        String sqlInsert = "INSERT INTO TRANSACOES " +
-                "(USUARIO_ID, DATA, DESCRICAO, VALOR, TIPO, CATEGORIA)" +
-                " VALUES (?, ?, ?, ?, ?, ?)";
-        PreparedStatement stmt = conexao.prepareStatement(sqlInsert);
+    public void insert(Connection conexao, Transacao transacao) {
+        try {
+            // Preparando o sql insert
+            String sqlInsert = "INSERT INTO TRANSACOES " +
+                    "(USUARIO_ID, DATA, DESCRICAO, VALOR, TIPO, CATEGORIA)" +
+                    " VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement stmt = conexao.prepareStatement(sqlInsert);
 
-        // Inserindo os valores nas mascaras do insert
-        stmt.setInt(1, 1); // TODO: por hora nao usando um Usuario real, dito que foi pedido para nao fazemos para a tabela de usuarios ainda
-        stmt.setDate(2, new Date(System.currentTimeMillis()));
-        stmt.setString(3,  transacao.getDescricao());
-        stmt.setDouble(4, transacao.getValor());
-        stmt.setString(5, transacao.getTipo());
-        stmt.setString(6, transacao.getCategoria());
+            // Inserindo os valores nas mascaras do insert
+            stmt.setInt(1, 1); // TODO: por hora nao usando um Usuario real, dito que foi pedido para nao fazemos para a tabela de usuarios ainda
+            stmt.setDate(2, new Date(System.currentTimeMillis()));
+            stmt.setString(3,  transacao.getDescricao());
+            stmt.setDouble(4, transacao.getValor());
+            stmt.setString(5, transacao.getTipo());
+            stmt.setString(6, transacao.getCategoria());
 
-        // TODO: adicionar try catch para tabela inexistente ou erro inserindo
-        stmt.executeUpdate();
-        System.out.println("Record inserted successfully!");
+            stmt.executeUpdate();
+            System.out.println("Record inserted successfully!");
+        } catch (SQLException exception) {
+            System.err.println("Algo deu errado ao tentar inserir uma transacao");
+            exception.printStackTrace();
+        }
     }
 
-    public ResultSet getAll(Connection conexao) throws SQLException {
-        // Preparando o sql all
-        String sqlSelect = "SELECT * FROM TRANSACOES";
-        PreparedStatement stmt = conexao.prepareStatement(sqlSelect);
+    public ResultSet getAll(Connection conexao) {
+        ResultSet resultSet = null;
+        try {
+            // Preparando o sql all
+            String sqlSelect = "SELECT * FROM TRANSACOES";
+            PreparedStatement stmt = conexao.prepareStatement(sqlSelect);
 
-        // TODO: adicionar try catch para tabela inexistente ou erro selecionando
-        ResultSet resultSet = stmt.executeQuery();
+            resultSet = stmt.executeQuery();
+        } catch (SQLException exception) {
+            System.err.println("Algo deu errado ao tentar selecionar todas as transacoes");
+            exception.printStackTrace();
+        }
         return resultSet;
     }
 }
