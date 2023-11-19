e9f6ef541b55ae80936b8cdc3aa7576feca781b7
pedroluiznogueira <peluiznogueiram@hotmail.com>
2023-11-19 13:53:41 -0300

    set up apache tomcat as maven plugin to spin up a servlet

--- src/main/java/fintech/servlet/MeuServlet.java
+++ src/main/java/fintech/servlet/MeuServlet.java
@@ -1,7 +1,29 @@
 package fintech.servlet;
 
-public class MeuServlet {
-    public static void main(String[] args) {
-        System.out.println("...");
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/exemplo")
+public class MeuServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Servlet is running...");
+        super.init();
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Servlet is destroyed...");
+        super.destroy();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().write("Hello world");
     }
 }
