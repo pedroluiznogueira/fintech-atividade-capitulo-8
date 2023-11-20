b0c34b13c6087b59529f978523e33712cff5099d
pedroluiznogueira <peluiznogueiram@hotmail.com>
2023-11-20 16:23:36 -0300

    fetch total of investments

--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -33,4 +33,97 @@ function mostrarDiv(id) {
   }
   let div = document.querySelector(id);
   div.style.display = 'block';
+
+  switch (id) {
+    case '#dashboard-investimento':
+        getQuantidadeDeInvestimentos();
+        break;
+    case '#dashboard-recebimento':
+      getQuantidadeDeRecebimentos();
+      break;
+    case '#dashboard-gasto':
+      getQuantidadeDeGastos();
+      break;
+  }
+}
+
+function getQuantidadeDeInvestimentos() {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        var jsonResponse = JSON.parse(xhr.responseText);
+        console.log('Quantidade de Investimentos:', jsonResponse.count);
+
+        mostrarQuantidadeDeInvestimentos(jsonResponse.count);
+      } else {
+        console.error('Error fetching data:', xhr.status, xhr.statusText);
+      }
+    }
+  };
+
+  xhr.open('GET', '/investimentos', true);
+  xhr.send();
+}
+
+function mostrarQuantidadeDeInvestimentos(quantidadeDeInvestimentos) {
+  var h4Element = document.querySelector('#dashboard-investimento h4');
+  if (h4Element) {
+    h4Element.innerHTML += ': ' + quantidadeDeInvestimentos;
+  }
+}
+
+function getQuantidadeDeRecebimentos() {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        var jsonResponse = JSON.parse(xhr.responseText);
+        console.log('Quantidade de Recebimentos:', jsonResponse.count);
+
+        mostrarQuantidadeDeRecebimentos(jsonResponse.count);
+      } else {
+        console.error('Error fetching data:', xhr.status, xhr.statusText);
+      }
+    }
+  };
+
+  xhr.open('GET', '/recebimentos', true);
+  xhr.send();
+}
+
+function mostrarQuantidadeDeRecebimentos(quantidadeDeRecebimentos) {
+  var h4Element = document.querySelector('#dashboard-recebimento h4');
+  if (h4Element) {
+    h4Element.innerHTML += ': ' + quantidadeDeRecebimentos;
+  }
+}
+
+function getQuantidadeDeGastos() {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        var jsonResponse = JSON.parse(xhr.responseText);
+        console.log('Quantidade de Gastos:', jsonResponse.count);
+
+        mostrarQuantidadeDeGastos(jsonResponse.count);
+      } else {
+        console.error('Error fetching data:', xhr.status, xhr.statusText);
+      }
+    }
+  };
+
+  xhr.open('GET', '/gastos', true);
+  xhr.send();
+}
+
+function mostrarQuantidadeDeGastos(quantidadeDeGastos) {
+  var h4Element = document.querySelector('#dashboard-gasto h4');
+  if (h4Element) {
+    h4Element.innerHTML += ': ' + quantidadeDeGastos;
+  }
 }
